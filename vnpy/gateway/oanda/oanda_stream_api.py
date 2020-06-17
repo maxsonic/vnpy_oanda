e90651e24b2555fef01cc7f855e4a4d1c35c16f6
maxSonic <maxsonic@outlook.com>
2020-06-17 18:56:05 +0800

    fix the pricing round issue

--- vnpy/gateway/oanda/oanda_stream_api.py
+++ vnpy/gateway/oanda/oanda_stream_api.py
@@ -153,17 +153,20 @@ class OandaStreamApi(OandaApiBase):
             # only one level of bids/asks
             bid = data['bids'][0]
             ask = data['asks'][0]
+            float_point = len(bid['price'].split(".")[1])
+            round_add = 1 / float_point * 10 * 5
             tick = TickData(
                 gateway_name=self.gateway_name,
                 symbol=symbol,
                 exchange=Exchange.OANDA,
                 datetime=parse_datetime(data['time']),
                 name=symbol,
-                last_price=(float(bid['price'])+float(ask['price'])) / 2,
+                last_price=round((float(bid['price'])+float(ask['price'] + round_add)) / 2),
                 bid_price_1=float(bid['price']),
                 bid_volume_1=bid['liquidity'],
                 ask_price_1=float(ask['price']),
                 ask_volume_1=ask['liquidity'],
+                volume=round((float(ask['liquidity'])+float(bid['liquidity']) + 0.5)/ 2),
             )
             self.gateway.on_tick(tick)
         self.latest_stream_time[symbol] = datetime.now()
